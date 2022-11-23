6080a33a865a55526337e30ed95411349b822ec8
Aenã <aenamartinelli@gmail.com>
2022-11-23 04:47:18 -0300

    logs-env-vars

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import { config } from 'dotenv'
-config();
 
 const Api = axios.create({ baseURL: process.env.API_URL || 'http://localhost:2002' });
-console.log(process.env.API_URL)
+
 export default Api;
