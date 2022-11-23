9a6dbcee533d0eed71dacd485403a1f6c5d9d501
Aenã <aenamartinelli@gmail.com>
2022-11-23 05:18:12 -0300

    change-api-to-heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-const Api = axios.create({ baseURL: process.env.API_URL || 'http://18.230.130.179:2002' });
+const Api = axios.create({ baseURL: process.env.API_URL || 'https://verzel-backend.herokuapp.com' });
 export default Api;
